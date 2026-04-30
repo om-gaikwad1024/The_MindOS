7901584c20d4e04a26519a85434d3cd61963a030
Om Gaikwad <om.gaikwad1024@gmail.com>
2026-04-30 17:38:31 +0530

    add force-dynamic to all api routes

--- app/api/progress/route.ts
+++ app/api/progress/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import { prisma, USER_ID } from "@/lib/prisma";
 import { subDays, isSameDay } from "date-fns";
